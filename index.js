1bfbd7fa0d09872cc794295a0911ff9366c1d861
EC2 Default User <ec2-user@ip-172-31-37-172.us-west-2.compute.internal>
2015-02-19 03:08:23 +0000

    Serves static html and css

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+	res.sendfile('build/index.html')
 })
-
+app.use("/css", express.static(__dirname + '/build/css'))
 var server = app.listen(8080, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+	var host = server.address().address
+	var port = server.address().port
 
   console.log('Example app listening at http://%s:%s', host, port)
 
